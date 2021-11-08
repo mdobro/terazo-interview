559b16116d0089c6f43d9a4ba8689537c7d56223
mdobroggg <dobro.michael@gmail.com>
2021-11-08 17:19:20 -0500

    fix lint

--- src/Components/WarehouseTable/PureWarehouseTable.jsx
+++ src/Components/WarehouseTable/PureWarehouseTable.jsx
@@ -1,6 +1,7 @@
 import "./PureWarehouseTable.css";
 
 import React from "react";
+import PropTypes from "prop-types";
 import { Table } from "reactstrap";
 import WarehouseTableRow from "../WarehouseTableRow/WarehouseTableRow";
 
@@ -37,8 +38,30 @@ const PureWarehouseTable = ({ warehouses, onRowChange, onRowDelete }) => (
   </Table>
 );
 
-PureWarehouseTable.propTypes = {};
+PureWarehouseTable.propTypes = {
+  warehouses: PropTypes.arrayOf(
+    PropTypes.shape({
+      warehouseId: PropTypes.string,
+      warehouseName: PropTypes.string,
+      warehouseDescription: PropTypes.string,
+      warehouseAddress: PropTypes.shape({
+        buildingName: PropTypes.string,
+        streetLine1: PropTypes.string,
+        streetLine2: PropTypes.string,
+        city: PropTypes.string,
+        stateProvince: PropTypes.string,
+        zipPostalCode: PropTypes.string,
+        country: PropTypes.string,
+      }).isRequired,
+    }).isRequired
+  ).isRequired,
+  onRowChange: PropTypes.func,
+  onRowDelete: PropTypes.func,
+};
 
-PureWarehouseTable.defaultProps = {};
+PureWarehouseTable.defaultProps = {
+  onRowChange: undefined,
+  onRowDelete: undefined,
+};
 
 export default PureWarehouseTable;
